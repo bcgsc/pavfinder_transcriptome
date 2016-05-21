11144738d290dda89786d76617496f1991058a76
Readman Chiu <rchiu@bcgsc.ca>
2016-05-20 17:05:39 -0700

    second draft

--- pavfinder_transcriptome/scripts/tap.py
+++ pavfinder_transcriptome/scripts/tap.py
@@ -8,6 +8,8 @@ import gzip
 import os
 #from ruffus.combinatorics import *
 import sys
+import glob
+import re
 
 def run_cmd(cmd):
     process = subprocess.Popen(cmd,
@@ -15,9 +17,14 @@ def run_cmd(cmd):
                                stderr = subprocess.PIPE,
                                shell = True)
     stdout_str, stderr_str = process.communicate()
-    if process.returncode != 0:
-        raise Exception("Failed to run '%s'\n%s%sNon-zero exit status %s" %
-                        (cmd, stdout_str, stderr_str, process.returncode))
+    if process.returncode != 0:     
+        if re.search('error: no contigs assembled', stderr_str):
+            return stderr_str
+        #elif re.search('exit', stderr_str):
+            #return stderr_str
+        else:
+            raise Exception("Failed to run '%s'\n%s%sNon-zero exit status %s" %
+                            (cmd, stdout_str, stderr_str, process.returncode))
     
 def format_read_pairs(fqs=None, list_file=None):
     fastqs = []
@@ -43,10 +50,15 @@ parser.add_argument('sample', type=str, help='sample name')
 parser.add_argument('outdir', type=str, help='output directory')
 parser.add_argument('--bf', type=str, help='path to bloomfilter')
 parser.add_argument('--fq', type=str, nargs='+', help='input gzipped fastqs')
-parser.add_argument('--fq_list', type=str, nargs='+', help='text file of input fastq paths')
+parser.add_argument('--fq_list', type=str, help='text file of input fastq paths')
 parser.add_argument('--nprocs', type=int, default=64, help='number of threads/processes')
 parser.add_argument('--k', type=int, nargs='+', help='k sizes for assembly')
 parser.add_argument('--readlen', type=int, help='read length')
+parser.add_argument('--gmap_index', type=str, nargs=2, help='gmap index')
+parser.add_argument('--bwa_index', type=str, help='bwa index of transcript sequences')
+parser.add_argument('--gtf', type=str, help='gtf')
+parser.add_argument('--sort_mem', type=str, help='samtools sort memory. Default:5G', default='5G')
+parser.add_argument('--genome_fasta', type=str, help='genome fasta')
 
 args = parser.parse_args()
 logger, logger_mutex = cmdline.setup_logging (__name__,
@@ -65,14 +77,15 @@ elif args.fq_list:
 bbt_outdir = '%s/bbt_v3.0.0b' % args.outdir
 assembly_outdir = '%s/transabyss_v1.5.4' % args.outdir
 pvt_outdir = '%s/pvt_v0.3.0' % args.outdir
-if not os.path.exists(bbt_outdir):
-    os.mkdir(bbt_outdir)
+#if not os.path.exists(bbt_outdir):
+    #os.mkdir(bbt_outdir)
 
 bbt_prefix = bbt_outdir + '/' + args.sample
 
 assembly_input = []
 
 #@follows('check_dir', mkdir(args.outdir))
+@follows(mkdir(bbt_outdir))
 @transform([read_pairs],
            formatter('_1.fastq.gz$'),
            bbt_outdir + '/' + args.sample + '_reads.fq',
@@ -159,6 +172,7 @@ def pair_reads(fastqs, pair):
     print 'ggg', fastqs
     pass
 
+@follows(mkdir(assembly_outdir))
 @subdivide(pair_reads,
            formatter(".+/(.+)_1.fastq.gz"),
            assembly_outdir + "/{1[0]}/k*",
@@ -193,20 +207,324 @@ def symlink_assembly_input(read_pairs, k_dirs, ks):
         print 'ggg', d
                 
 @transform(symlink_assembly_input,
-           formatter(".+/(.+)/k\d+"),
-           "{0[0]}/{1[0]}-final.fa")
+           formatter(".+/(.+)/(k\d+)"),
+           assembly_outdir + "/{1[0]}/{2[0]}/{1[0]}-final.fa")
+           #"{0[0]}/{1[0]}-final.fa")
 def assemble_single_gene(k_dir, contigs_file):
     gene, k = filter(None, k_dir.split(os.sep))[-2:]
-    print 'ccc', gene, k
+    #print 'ccc', gene, k
     
     cmd = 'transabyss --kmer %s --pe %s %s --outdir %s --name %s --cleanup 3' % (k.lstrip('k'),
                                                                                  '%s/%s_1.fastq.gz' % (k_dir, gene),
                                                                                  '%s/%s_2.fastq.gz' % (k_dir, gene),
                                                                                  k_dir,
                                                                                  gene)
+    error = run_cmd(cmd)
+    if error and os.path.exists('%s/coverage.hist' % k_dir):
+        run_cmd('touch %s/%s-final.fa %s/%s-FINAL.COMPLETE' % (k_dir, gene, k_dir, gene))
+    
+@collate(assemble_single_gene,
+         formatter(".+/k\d+/(.+)-final.fa$"),
+         assembly_outdir + "/{1[0]}/{1[0]}-merged.fa",
+         args.readlen)
+         #"{subdir[0][:-2]/{1[0]}-merged.fa")
+def merge_assemblies(k_assemblies, merged_fasta, readlen):
+    ks = []
+    for k_assembly in k_assemblies:
+        gene, k = filter(None, k_assembly.split(os.sep))[-3:-1]
+        ks.append(int(k.lstrip('k')))
+    
+    prefixes = ' '.join(['%s.k%d.' % (gene, k) for k in ks])
+    
+    merged_fasta = '/'.join(filter(None, k_assemblies[0].split(os.sep))[:-2]) + '/%s-merged.fa' % gene
+    
+    # check if we have all empty assemblies
+    num_empty_assemblies = 0
+    for assembly in k_assemblies:
+        if os.path.getsize(assembly) == 0:
+            num_empty_assemblies += 1
+    
+    if num_empty_assemblies < len(k_assemblies):
+        cmd = 'transabyss-merge --mink %d --maxk %d --prefixes %s --length %d %s --out %s --force' % (min(ks),
+                                                                                                      max(ks),
+                                                                                                      prefixes,
+                                                                                                      readlen,
+                                                                                                      ' '.join(k_assemblies),
+                                                                                                      merged_fasta)
+    else:
+        cmd = 'touch %s' % merged_fasta
+
+    run_cmd(cmd)
+    
+@transform(merge_assemblies,
+           formatter(".+-merged.fa"),
+           "{0[0]}.bwt")
+def r2c_bwa_index(merged_fasta, index):
+    if os.path.getsize(merged_fasta) > 0:
+        cmd = 'bwa index %s' % merged_fasta
+
+    else:
+        cmd = 'touch %s' % index
+        
+    run_cmd(cmd)
+    
+@transform(r2c_bwa_index,
+           formatter(),
+           "{path[0]}/r2c.bam",
+           args.nprocs,
+           args.sort_mem)
+def r2c(index, r2c_bam, nthreads, sort_mem):
+    gene = filter(None, index.split(os.sep))[-2]
+    reads1 = '%s/%s_1.fastq.gz' % (bbt_outdir, gene)
+    reads2 = '%s/%s_2.fastq.gz' % (bbt_outdir, gene)
+    
+    if os.path.getsize(index) > 0:
+        cmd = 'bwa mem -t %d %s %s %s | samtools view -uhS - | samtools sort -m %s - %s' % (nthreads,
+                                                                                            os.path.splitext(index)[0],
+                                                                                            reads1,
+                                                                                            reads2,
+                                                                                            sort_mem,
+                                                                                            os.path.splitext(r2c_bam)[0])
+        print 'ooo', cmd
+        #run_cmd('/bin/bash -c "%s"' % cmd)
+        
+    else:
+        cmd = 'touch %s' % r2c_bam
+        
+    run_cmd('/bin/bash -c "%s"' % cmd)
+    
+#@transform(r2c_bwa_index,
+           #formatter(),
+           #"{path[0]}/r2c.bam",
+           #args.nprocs)
+#def r2c_aln(index, r2c_bam, nthreads):
+    #gene = filter(None, index.split(os.sep))[-2]
+    #reads1 = '%s/%s_1.fastq.gz' % (bbt_outdir, gene)
+    #reads2 = '%s/%s_2.fastq.gz' % (bbt_outdir, gene)
+    #r2c_bam = os.path.dirname(index) + '/r2c.bam'
+    
+    #cmd = 'bwa mem -t %d %s %s %s | samtools view -bhS - -o %s' % (nthreads,
+                                                                   #os.path.splitext(index)[0],
+                                                                   #reads1,
+                                                                   #reads2,
+                                                                   #r2c_bam)
+    #print 'ooo', cmd
+    #run_cmd('/bin/bash -c "%s"' % cmd)
+    
+#@transform(r2c_aln,
+           #suffix('.bam'),
+           #'_ns.bam')
+#def r2c_name_sort(r2c_bam, r2c_ns_bam):
+    #cmd = 'samtools sort -n %s %s' % (r2c_bam,
+                                      #os.path.splitext(r2c_bam)[0] + '_ns')
+    
+    #print cmd
+    #run_cmd(cmd)
+
+#@transform(r2c_name_sort,
+           #suffix('_ns.bam'),
+           #'_fm.bam')
+#def r2c_fixmate(r2c_ns_bam, r2c_fm_bam):
+    #cmd = 'samtools fixmate %s %s' % (r2c_ns_bam,
+                                      #os.path.dirname(r2c_ns_bam) + '/r2c_fm.bam')
+    #print cmd
+    #run_cmd(cmd)                      
+
+#@transform(r2c_fixmate,
+           #suffix('_fm.bam'),
+           #'_sorted.bam')
+#def r2c_sort(r2c_fm_bam, r2c_sorted_bam):
+    #cmd = 'samtools sort %s %s' % (r2c_fm_bam,
+                                   #os.path.dirname(r2c_fm_bam) + '/r2c_sorted')
+    #print cmd
+    #run_cmd(cmd)
+
+#@transform(r2c_sort,
+           #suffix('_sorted.bam'),
+           #'_sorted.bam.bai')
+#def r2c_index(r2c_sorted_bam, r2c_sorted_bam_index):
+    #cmd = 'samtools index %s' % r2c_sorted_bam
+    #print cmd
+    #run_cmd(cmd)
+    
+    
+@merge(merge_assemblies,
+       '%s/%s.fa' % (assembly_outdir, args.sample))
+def concat_fasta(gene_fastas, single_merged_fasta):
+    cmd = 'cat %s > %s' % (' '.join(gene_fastas),
+                           single_merged_fasta)
+    run_cmd('/bin/bash -c "%s"' % cmd)
+    
+@merge(r2c,
+       '%s/r2c_cat.bam' % assembly_outdir,
+       args.sort_mem)
+def r2c_concat(r2c_bams, r2c_cat_bam, sort_mem):
+    def create_header(header_file):
+        with open(header_file, 'w') as out:
+            for i in range(len(r2c_bams)):
+                if os.path.getsize(r2c_bams[i]) == 0:
+                    continue
+                cmd = subprocess.Popen('samtools view -H %s' % r2c_bams[i], shell=True, stdout=subprocess.PIPE)
+                for line in cmd.stdout:
+                    if i == 0:
+                        if line[:3] == '@HD':
+                            out.write(line)
+                    if line[:3] == '@SQ':
+                        out.write(line)
+        
+    header_file = '%s/r2c_cat_header.sam' % assembly_outdir
+    if not os.path.exists(header_file) or os.path.getsize(header_file) == 0:
+        create_header(header_file)
+    
+    bams_str = ';'.join(['cat %s' %header_file] + ['samtools view ' + bam for bam in r2c_bams if os.path.getsize(bam) > 0])
+    
+    cmd = '{ %s; } | samtools view -Su - | samtools sort -m %s - %s' % (bams_str,
+                                                                       sort_mem,
+                                                                       os.path.splitext(r2c_cat_bam)[0])
     print cmd
-    run_cmd(cmd)    
+    run_cmd('/bin/bash -c "%s"' % cmd)
+    
+#@transform(r2c_concat,
+           #suffix('.bam'),
+           #'_sorted.bam',
+           #args.sort_mem)
+#def r2c_sort_concat(r2c_cat_bam, r2c_cat_sorted_bam, sort_mem):
+    #cmd = 'samtools sort -m %s %s %s' % (sort_mem,
+                                         #r2c_cat_bam,
+                                         #os.path.splitext(r2c_cat_sorted_bam)[0])
+    #print cmd
+    #run_cmd(cmd)
+
+@transform(r2c_concat,
+           suffix('.bam'),
+           '.bam.bai')
+def r2c_index_concat(r2c_cat_sorted_bam, r2c_cat_sorted_bam_index):
+    cmd = 'samtools index %s' % r2c_cat_sorted_bam
+    print cmd
+    run_cmd(cmd)
+    
+#@transform(r2c_index,
+           #formatter(),
+           #"{0[0]}")
+#def r2c_cleanup(index, output):                                                                                    
+    #gene, k = filter(None, index.split(os.sep))[-2]
+    #cmd = 'rm %s/%s-merged.fa.* %s/r2c.bam %s/r2c_fm.bam %s/r2c_ns.bam' % (os.path.dirname(index),
+                                                                           #gene,
+                                                                           #os.path.dirname(index),
+                                                                           #os.path.dirname(index),
+                                                                           #os.path.dirname(index))
+    #print cmd
+    
+@transform(concat_fasta,
+           formatter(".+.fa$"),
+           "{path[0]}/c2g.bam",
+           args.gmap_index,
+           args.nprocs)
+def c2g(contigs_fasta, c2g_bam, gmap_index, nthreads):
+    cmd = 'gmap -D %s -d %s %s -t %d -f samse -n 0 -x 10 | samtools view -bhS - -o %s' % (gmap_index[0],
+                                                                                          gmap_index[1],
+                                                                                          contigs_fasta,
+                                                                                          nthreads,
+                                                                                          c2g_bam)
+    run_cmd('/bin/bash -c "%s"' % cmd)
+    
+@transform(concat_fasta,
+           formatter(".+.fa$"),
+           "{path[0]}/c2t.bam",
+           args.bwa_index,
+           args.nprocs)
+def c2t(contigs_fasta, c2t_bam, bwa_index, nthreads):
+    cmd = 'bwa mem -t %d %s %s | samtools view -bhS - -o %s' % (nthreads,
+                                                                bwa_index,
+                                                                contigs_fasta,
+                                                                c2t_bam)
+    run_cmd('/bin/bash -c "%s"' % cmd)
+    
+@follows(mkdir(pvt_outdir))
+@merge([concat_fasta, c2g, c2t, r2c_index_concat],
+       pvt_outdir + '/events.bedpe',
+       args.bwa_index,
+       args.gmap_index,
+       args.gtf,
+       args.genome_fasta)
+def find_events(inputs, events_output, transcripts_index, gmap_index, gtf, genome_fasta):
+    merged_fasta, c2g_bam, c2t_bam, r2c_index = inputs
+    cmd = 'find_events.py --gbam %s --tbam %s --transcripts_fasta %s --genome_index %s --r2c %s %s %s %s %s' % (c2g_bam,
+                                                                                                                c2t_bam,
+                                                                                                                transcripts_index,
+                                                                                                                ' '.join(gmap_index),
+                                                                                                                os.path.splitext(r2c_index)[0],
+                                                                                                                merged_fasta,
+                                                                                                                gtf,
+                                                                                                                genome_fasta,
+                                                                                                                os.path.dirname(events_output)
+                                                                                                                )
+    print cmd
+    run_cmd(cmd)
+           
+    
+#@transform(concat_fasta,
+           #formatter(".+.fa$"),
+           #"{0[0]}.bwt")
+#def r2c_bwa_index(merged_fasta, index):
+    #print 'ttt', merged_fasta
+    #cmd = 'bwa index %s' % merged_fasta
+    #run_cmd(cmd)
+    
+#@transform(r2c_bwa_index,
+           #formatter(),
+           #"{path[0]}/r2c.bam",
+           #args.nprocs)
+#def r2c_aln(index, r2c_bam, nthreads):
+    #reads1 = glob.glob('%s/*_1.fastq.gz' % bbt_outdir)
+    #reads2 = glob.glob('%s/*_2.fastq.gz' % bbt_outdir)
+    #r2c_bam = os.path.dirname(index) + '/r2c.bam'
+    
+    #cmd = 'bwa mem -t %d %s %s %s | samtools view -bhS - -o %s' % (nthreads,
+                                                                   #os.path.splitext(index)[0],
+                                                                   #'<(zcat %s)' % ' '.join(reads1),
+                                                                   #'<(zcat %s)' % ' '.join(reads2),
+                                                                   #r2c_bam)
+    #print 'ooo', cmd
+    #run_cmd('/bin/bash -c "%s"' % cmd)
+    
+#@transform(r2c_aln,
+           #suffix('.bam'),
+           #'_ns.bam')
+#def r2c_name_sort(r2c_bam, r2c_ns_bam):
+    #cmd = 'samtools sort -n %s %s' % (r2c_bam,
+                                      #os.path.splitext(r2c_bam)[0] + '_ns')
+    
+    #print cmd
+    #run_cmd(cmd)
+
+#@transform(r2c_name_sort,
+           #suffix('_ns.bam'),
+           #'_fm.bam')
+#def r2c_fixmate(r2c_ns_bam, r2c_fm_bam):
+    #cmd = 'samtools fixmate %s %s' % (r2c_ns_bam,
+                                      #os.path.dirname(r2c_ns_bam) + '/r2c_fm.bam')
+    #print cmd
+    #run_cmd(cmd)                      
+
+#@transform(r2c_fixmate,
+           #suffix('_fm.bam'),
+           #'_sorted.bam')
+#def r2c_sort(r2c_fm_bam, r2c_sorted_bam):
+    #cmd = 'samtools sort %s %s' % (r2c_fm_bam,
+                                   #os.path.dirname(r2c_fm_bam) + '/r2c_sorted')
+    #print cmd
+    #run_cmd(cmd)
+
+#@transform(r2c_sort,
+           #suffix('_sorted.bam'),
+           #'_sorted.bam.bai')
+#def r2c_index(r2c_sorted_bam, r2c_sorted_bam_index):
+    #cmd = 'samtools index %s' % r2c_sorted_bam
+    #print cmd
+    #run_cmd(cmd)
+
 
 pipeline_printout(sys.stdout, verbose=3)
-pipeline_run(verbose=3, multiprocess = 15)
+pipeline_run(verbose=3, multiprocess = args.nprocs)
 #pipeline_run([assemble], verbose=2, multiprocess = 5)
