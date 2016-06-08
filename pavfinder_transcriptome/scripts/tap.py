b9da44e78ef410ff21c2f1c8cdd05b1b0d59dd00
Readman Chiu <rchiu@bcgsc.ca>
2016-06-08 09:40:40 -0700

    can now run both targeted and non-targeted ways

--- pavfinder_transcriptome/scripts/tap.py
+++ pavfinder_transcriptome/scripts/tap.py
@@ -95,37 +95,65 @@ assembly_input = []
            bbt_prefix,
            args.bf,
            args.nprocs)
-def classify(paired_fqs, bbt_output, prefix, bf, nthreads):  
-    if len(paired_fqs[0]) == 1:
-        input_fq1 = paired_fqs[0][0]
-    else:
-        input_fq1 = '<(zcat %s)' % ' '.join(paired_fqs[0])
-    if len(paired_fqs[1]) == 1:
-        input_fq2 = paired_fqs[1][0]
-    else:
-        input_fq2 = '<(zcat %s)' % ' '.join(paired_fqs[1])
-    
-    cmd = 'biobloomcategorizer --fq -i -p %s -a 2 -t %d -e -f %s %s %s' % (prefix,
-                                                                           nthreads,
-                                                                           bf,
-                                                                           input_fq1,
-                                                                           input_fq2)
+def classify(paired_fqs, bbt_output, prefix, bf, nthreads):
+    if bf:
+        if len(paired_fqs[0]) == 1:
+            input_fq1 = paired_fqs[0][0]
+        else:
+            input_fq1 = '<(zcat %s)' % ' '.join(paired_fqs[0])
+        if len(paired_fqs[1]) == 1:
+            input_fq2 = paired_fqs[1][0]
+        else:
+            input_fq2 = '<(zcat %s)' % ' '.join(paired_fqs[1])
         
+        cmd = 'biobloomcategorizer --fq -i -p %s -a 2 -t %d -e -f %s %s %s' % (prefix,
+                                                                               nthreads,
+                                                                               bf,
+                                                                               input_fq1,
+                                                                               input_fq2)
+    else:
+        cmd = 'touch %s' % bbt_output
+
     run_cmd('/bin/bash -c "%s"' % cmd)
     
 @split(classify,
-       bbt_outdir + '/*.fastq')
+       bbt_outdir + '/*.fastq*')
 def split_input(bbt_fastq, split_fastqs, genes=None):
     seqs1 = defaultdict(list)
     seqs2 = defaultdict(list)
 
-    with open(bbt_fastq[0], 'r') as fq:
-        for lines in itertools.izip_longest(*[fq]*8):
-            target = lines[0].rstrip().split(' ')[1].split('.')[0]
-            seqs1[target].extend(lines[:4])
-            seqs2[target].extend(lines[-4:])
+    if args.bf:
+        with open(bbt_fastq[0], 'r') as fq:
+            for lines in itertools.izip_longest(*[fq]*8):
+                target = lines[0].rstrip().split(' ')[1].split('.')[0]
+                seqs1[target].extend(lines[:4])
+                seqs2[target].extend(lines[-4:])
 
-    split_fastqs = output_split_pairs(seqs1, seqs2, bbt_outdir)
+        split_fastqs = output_split_pairs(seqs1, seqs2, bbt_outdir)
+
+    else:
+        for i in range(len(read_pairs[0])):
+            source = os.path.abspath(read_pairs[0][i])
+            if len(read_pairs[0]) == 1:
+                target = '%s/%s_%d.fastq' % (bbt_outdir, args.sample, 1)
+            else:
+                target = '%s/%s_%d-%d.fastq' % (bbt_outdir, args.sample, 1, i + 1)
+            if os.path.splitext(source)[1] == '.gz':
+                target += '.gz'
+
+            if not os.path.exists(target):
+                os.symlink(source, target)
+
+            source = os.path.abspath(read_pairs[1][i])
+            if len(read_pairs[1]) == 1:
+                target = '%s/%s_%d.fastq' % (bbt_outdir, args.sample, 2)
+            else:
+                target = '%s/%s_%d-%d.fastq' % (bbt_outdir, args.sample, 2, i + 1)
+            if os.path.splitext(source)[1] == '.gz':
+                target += '.gz'
+
+            if not os.path.exists(target):
+                os.symlink(source, target)
 
 def output_split_pairs(seqs1, seqs2, outdir):
     fqs = []
@@ -144,52 +172,65 @@ def output_split_pairs(seqs1, seqs2, outdir):
     return fqs
 
 @collate(split_input,
-         formatter(".+/(.+)_[12].fastq"),
-         ["{path[0]}/{1[0]}_1.fastq",
-          "{path[0]}/{1[0]}_2.fastq"])
-def pair_reads(fastqs, pair):
-    pass
+         formatter(".+/(.+?)_[12]-*\d*.fastq.*"),
+         assembly_outdir + "/{1[0]}/{1[0]}.in")
+def pair_reads(fastqs, input_file):
+    outdir = os.path.dirname(input_file)
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
 
-@follows(mkdir(assembly_outdir))
+    fastqs_sorted = []
+    if len(fastqs) == 2:
+        fastqs_sorted = sorted(fastqs)
+    elif len(fastqs) > 2:
+        fastqs_batches = defaultdict(list)
+        batch = re.compile('_[12](-\d+)\.')
+        for fastq in sorted(fastqs):
+            match = batch.search(os.path.basename(fastq))
+            if not match:
+                fastqs_sorted.append(fastq)
+            else:
+                fastqs_batches[match.group(1)].append(fastq)
+        if fastqs_batches.keys():
+            for batch in sorted(fastqs_batches.keys()):
+                for fastq in fastqs_batches[batch]:
+                    fastqs_sorted.append(fastq)
+
+    if fastqs_sorted:
+        with open(input_file, 'w') as out:
+            for fastq in fastqs_sorted:
+                out.write(os.path.abspath(fastq) + '\n')
+
 @subdivide(pair_reads,
-           formatter(".+/(.+)_1.fastq"),
-           assembly_outdir + "/{1[0]}/k*",
+           formatter(),
+           "{path[0]}/k*",
            args.k)
-def symlink_assembly_input(read_pairs, k_dirs, ks):  
-    gene = os.path.basename(read_pairs[0]).split('.')[0].split('_')[0]
-    linked_reads = []
-    
+def symlink_assembly_input(assembly_input, k_dirs, ks):
+    prefix = os.path.splitext(os.path.basename(assembly_input))[0]
     for k in ks:
-        k_dir = '%s/%s/k%d' % (assembly_outdir, gene, k)
-        k_dirs.append(k_dir)
+        k_dir = '%s/%s/k%d' % (assembly_outdir, prefix, k)
         if not os.path.exists(k_dir):
             os.makedirs(k_dir)
-        
-        for i in range(2):
-            source = os.path.relpath(read_pairs[i], k_dir)
-            target = '%s/%s_%d.fastq' % (k_dir, gene, i + 1)
-            linked_reads.append(target)
-        
-            if not os.path.exists(target):
-                os.symlink(source, target)
+
+        os.symlink(os.path.relpath(assembly_input, k_dir), '%s/%s' % (k_dir, os.path.basename(assembly_input)))
                 
 @transform(symlink_assembly_input,
            formatter(".+/(.+)/(k\d+)"),
            assembly_outdir + "/{1[0]}/{2[0]}/{1[0]}-final.fa",
            logger, logging_mutex)
 def assemble_single_gene(k_dir, contigs_file, logger, logging_mutex):
-    gene, k = filter(None, k_dir.split(os.sep))[-2:]
+    prefix, k = filter(None, k_dir.split(os.sep))[-2:]
+    fastqs = ' '.join(open('%s/%s.in' % (k_dir, prefix), 'r').read().split('\n')[:-1])
     
-    cmd = 'transabyss --kmer %s --pe %s %s --outdir %s --name %s --cleanup 3' % (k.lstrip('k'),
-                                                                                 '%s/%s_1.fastq' % (k_dir, gene),
-                                                                                 '%s/%s_2.fastq' % (k_dir, gene),
-                                                                                 k_dir,
-                                                                                 gene)
+    cmd = 'transabyss --kmer %s --pe %s --outdir %s --name %s --cleanup 3' % (k.lstrip('k'),
+                                                                              fastqs,
+                                                                              k_dir,
+                                                                              prefix)
     stdout_str, stderr_str = run_cmd(cmd, force=True)
 
     if stderr_str and\
        re.search('error', stderr_str, re.IGNORECASE):
-        run_cmd('touch %s/%s-final.fa %s/%s-FINAL.COMPLETE' % (k_dir, gene, k_dir, gene))
+        run_cmd('touch %s/%s-final.fa %s/%s-FINAL.COMPLETE' % (k_dir, prefix, k_dir, prefix))
         with logging_mutex:
             logger.info(stderr_str)
     
@@ -203,7 +244,10 @@ def merge_assemblies(k_assemblies, merged_fasta, readlen):
         gene, k = filter(None, k_assembly.split(os.sep))[-3:-1]
         ks.append(int(k.lstrip('k')))
     
-    prefixes = ' '.join(['%s.k%d.' % (gene, k) for k in ks])
+    if args.bf:
+        prefixes = ' '.join(['%s.k%d.' % (gene, k) for k in ks])
+    else:
+        prefixes = ' '.join(['k%d.' % k for k in ks])
     
     merged_fasta = '/'.join(filter(None, k_assemblies[0].split(os.sep))[:-2]) + '/%s-merged.fa' % gene
     
@@ -243,66 +287,98 @@ def r2c_bwa_index(merged_fasta, index):
            args.nprocs,
            args.sort_mem)
 def r2c(index, r2c_bam, nthreads, sort_mem):
-    gene = filter(None, index.split(os.sep))[-2]
-    reads1 = '%s/%s_1.fastq' % (bbt_outdir, gene)
-    reads2 = '%s/%s_2.fastq' % (bbt_outdir, gene)
+    # extract reads1 and reads2 paths
+    prefix = os.path.basename(os.path.dirname(index))
+    reads1 = []
+    reads2 = []
+    with open('%s/%s.in' % (os.path.dirname(index), prefix)) as ff:
+        for line in ff:
+            if prefix + '_1' in os.path.basename(line):
+                reads1.append(line.rstrip())
+            elif prefix + '_2' in os.path.basename(line):
+                reads2.append(line.rstrip())
+
+    if len(reads1) > 1:
+        if os.path.splitext(reads1[0])[1] == '.gz':
+            reads1_str = '<(zcat %s)' % ' '.join(reads1)
+        elif os.path.splitext(reads1[0])[1].lower() in ('.fastq', '.fq'):
+            reads1_str = '<(cat %s)' % ' '.join(reads1)
+    else:
+        reads1_str = reads1[0]
+
+    if len(reads2) > 1:
+        if os.path.splitext(reads2[0])[1] == '.gz':
+            reads2_str = '<(zcat %s)' % ' '.join(reads2)
+        elif os.path.splitext(reads2[0])[1].lower() in ('.fastq', '.fq'):
+            reads2_str = '<(cat %s)' % ' '.join(reads2)
+    else:
+        reads2_str = reads2[0]
     
     if os.path.getsize(index) > 0:
         cmd = 'bwa mem -t %d %s %s %s | samtools view -uhS - | samtools sort -m %s - %s' % (nthreads,
                                                                                             os.path.splitext(index)[0],
-                                                                                            reads1,
-                                                                                            reads2,
+                                                                                            reads1_str,
+                                                                                            reads2_str,
                                                                                             sort_mem,
                                                                                             os.path.splitext(r2c_bam)[0])
-        run_cmd('/bin/bash -c "%s"' % cmd)
-        
     else:
         cmd = 'touch %s' % r2c_bam
         
     run_cmd('/bin/bash -c "%s"' % cmd)
-    
+
 @merge(merge_assemblies,
        '%s/%s.fa' % (assembly_outdir, args.sample))
 def concat_fasta(gene_fastas, single_merged_fasta):
-    fin = fileinput.input(gene_fastas)
-    with open(single_merged_fasta, 'w') as fout:
-        for line in fin:
-            fout.write(line)
-    fin.close()
+    if args.bf:
+        fin = fileinput.input(gene_fastas)
+        with open(single_merged_fasta, 'w') as fout:
+            for line in fin:
+                fout.write(line)
+        fin.close()
+    else:
+        source = os.path.relpath(gene_fastas[0], os.path.dirname(single_merged_fasta))
+        os.symlink(source, single_merged_fasta)
     
 @merge(r2c,
-       '%s/r2c_cat.bam' % assembly_outdir,
+       '%s/r2c.bam' % assembly_outdir,
        args.sort_mem)
 def r2c_concat(r2c_bams, r2c_cat_bam, sort_mem):
     header_file = '%s/r2c_cat.header' % assembly_outdir
     sam_file = '%s/r2c_cat.sam' % assembly_outdir
-    with open(header_file, 'w') as header, open(sam_file, 'w') as sam:
-        for i in range(len(r2c_bams)):
-            if os.path.getsize(r2c_bams[i]) == 0:
-                    continue
-            cmd = subprocess.Popen('samtools view -H %s' % r2c_bams[i], shell=True, stdout=subprocess.PIPE)
-            for line in cmd.stdout:
-                if i == 0 and line[:3] == '@HD':
-                    header.write(line)
-                elif line[:3] == '@SQ':
-                    header.write(line)
-
-            cmd = subprocess.Popen('samtools view %s' % r2c_bams[i], shell=True, stdout=subprocess.PIPE)
-            sam.writelines(cmd.stdout)
-
-    cmd = 'cat %s %s | samtools view -Su - | samtools sort -m %s - %s' % (header_file,
-                                                                          sam_file,
-                                                                          sort_mem,
-                                                                          os.path.splitext(r2c_cat_bam)[0])
+    if args.bf and len(r2c_bams) > 1:
+        with open(header_file, 'w') as header, open(sam_file, 'w') as sam:
+            for i in range(len(r2c_bams)):
+                if os.path.getsize(r2c_bams[i]) == 0:
+                        continue
+                cmd = subprocess.Popen('samtools view -H %s' % r2c_bams[i], shell=True, stdout=subprocess.PIPE)
+                for line in cmd.stdout:
+                    if i == 0 and line[:3] == '@HD':
+                        header.write(line)
+                    elif line[:3] == '@SQ':
+                        header.write(line)
     
-    run_cmd('/bin/bash -c "%s"' % cmd)
+                cmd = subprocess.Popen('samtools view %s' % r2c_bams[i], shell=True, stdout=subprocess.PIPE)
+                sam.writelines(cmd.stdout)
+
+        cmd = 'cat %s %s | samtools view -Su - | samtools sort -m %s - %s' % (header_file,
+                                                                              sam_file,
+                                                                              sort_mem,
+                                                                              os.path.splitext(r2c_cat_bam)[0])
+
+        run_cmd('/bin/bash -c "%s"' % cmd)
+
+    elif not args.bf and len(r2c_bams) == 1:
+        source = os.path.relpath(r2c_bams[0], os.path.dirname(r2c_cat_bam))
+        os.symlink(source, r2c_cat_bam)
 
 def r2c_cleanup():
     temp_files = ['%s/r2c_cat.header' % assembly_outdir, '%s/r2c_cat.sam' % assembly_outdir]
     for ff in glob.glob('%s/*/*-merged.fa.*' % assembly_outdir):
         temp_files.append(ff)
-    for ff in glob.glob('%s/*/r2c.bam' % assembly_outdir):
-        temp_files.append(ff)
+
+    if args.bf:
+        for ff in glob.glob('%s/*/r2c.bam' % assembly_outdir):
+            temp_files.append(ff)
 
     for temp_file in temp_files:
         if os.path.exists(temp_file):
